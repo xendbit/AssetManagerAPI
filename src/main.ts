f3077d6896046ad2ffc49caf6618ae46720c9557
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-01-08 12:44:06 +0100

    Increase upload size.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
+  app.use(json({limit: '50mb'}));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.useGlobalInterceptors(new LoggerInterceptor());  
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ExceptionsFilter());
